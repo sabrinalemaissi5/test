a58b4a7057938ec57f3dff135726f4a7a4c569cf
sabrina 2002 <sabrinalemaissi5@gmail.com>
2025-11-30 09:56:16 +0100

    ahoutons les tables factures

--- models/example/test.sql
+++ models/example/test.sql
@@ -3,7 +3,39 @@
 ) }}
 
 SELECT
-   'AHS' as abh_id,
-    id as facture_id,
-    Unite_Id as usagers 
+    'AHS' AS abh_id,
+    id AS facture_id,
+    Unite_Id AS usagers
 FROM public."ahs_factures"
+
+UNION ALL
+
+SELECT
+    'SAHRA' AS abh_id,
+    id AS facture_id,
+    Unite_Id AS usagers
+FROM public."sahra_factures"
+
+UNION ALL
+
+SELECT
+    'CSM' AS abh_id,
+    id AS facture_id,
+    Unite_Id AS usagers
+FROM public."csm_factures"
+
+UNION ALL
+
+SELECT
+    'OCC' AS abh_id,
+    id AS facture_id,
+    Unite_Id AS usagers
+FROM public."occ_factures"
+
+UNION ALL
+
+SELECT
+    'CZ' AS abh_id,
+    id AS facture_id,
+    Unite_Id AS usagers
+FROM public."cz_factures";
